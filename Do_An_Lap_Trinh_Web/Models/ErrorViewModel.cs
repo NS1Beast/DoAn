6d67f2d8ad2b902725606e8f9de91e1a8472b78d
Lap trinh web Quan Li Chi Tieu <nguyenvuvinh1369@gmail.com>
2025-07-27 22:15:11 +0700

    Giao Diện Trang Giới thiệu

---
+++ Do_An_Lap_Trinh_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Do_An_Lap_Trinh_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
