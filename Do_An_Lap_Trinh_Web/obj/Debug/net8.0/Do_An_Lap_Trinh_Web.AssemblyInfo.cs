33b0d7ee7013e0d24a1c7eb4a21b1989a58cbc93
NS1Beast <fvgfv123nam@gmail.com>
2025-07-28 09:09:49 +0700

    File web da chinh sua

--- Do_An_Lap_Trinh_Web/obj/Debug/net8.0/Do_An_Lap_Trinh_Web.AssemblyInfo.cs
+++ Do_An_Lap_Trinh_Web/obj/Debug/net8.0/Do_An_Lap_Trinh_Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Do_An_Lap_Trinh_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d67f2d8ad2b902725606e8f9de91e1a8472b78d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Do_An_Lap_Trinh_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Do_An_Lap_Trinh_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
